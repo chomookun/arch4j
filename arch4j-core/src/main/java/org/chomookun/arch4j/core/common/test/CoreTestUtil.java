33f84c420fa39dc0625362d58a4f6f2ea511b924
chomookun <chomookun@gmail.com>
2025-11-09 16:51:47 +0900

    feat: adds read file as string method in CoreTestUtil

--- arch4j-core/src/main/java/org/chomookun/arch4j/core/common/test/CoreTestUtil.java
+++ arch4j-core/src/main/java/org/chomookun/arch4j/core/common/test/CoreTestUtil.java
@@ -1,13 +1,19 @@
 package org.chomookun.arch4j.core.common.test;
 
 import com.github.javaparser.utils.LineSeparator;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
+@Slf4j
 public class CoreTestUtil {
 
     /**
@@ -38,6 +44,21 @@ public class CoreTestUtil {
             });
             return stringBuilder.toString();
         } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Reads file as string
+     * @param filePath file path
+     * @return file content as string
+     */
+    public static String readFileAsString(String filePath) {
+        log.info("Working Directory: {}", System.getProperty("user.dir"));
+        try {
+            Path path = Paths.get(filePath);
+            return Files.readString(path, StandardCharsets.UTF_8);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
